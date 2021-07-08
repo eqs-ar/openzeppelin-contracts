2f0b60168f1c10736e14f370d50e2cf06d5251be
Guillermo Fabián Enriques <82797636+eqs-ar@users.noreply.github.com>
2021-07-08 20:32:27 -0300

    Update PaymentSplitter.sol

--- contracts/finance/PaymentSplitter.sol
+++ contracts/finance/PaymentSplitter.sol
@@ -8,16 +8,19 @@ import "../utils/math/SafeMath.sol";
 
 /**
  * @title PaymentSplitter
- * @dev This contract allows to split Ether payments among a group of accounts. The sender does not need to be aware
- * that the Ether will be split in this way, since it is handled transparently by the contract.
+ * @dev Este contrato permite dividir los pagos de Ether entre un grupo de cuentas. 
+ * El remitente no necesita estar al tanto que el Ether se dividirá de esta manera, 
+ * ya que el contrato lo maneja de manera transparente.
  *
- * The split can be in equal parts or in any other arbitrary proportion. The way this is specified is by assigning each
- * account to a number of shares. Of all the Ether that this contract receives, each account will then be able to claim
- * an amount proportional to the percentage of total shares they were assigned.
+ * La división puede ser en partes iguales o en cualquier otra proporción arbitraria. 
+ * The way this is specified is by assigning each account to a number of shares. 
+ * De todo el Ether que recibe este contrato, cada cuenta podrá reclamar una cantidad 
+ * proporcional al porcentaje del total de acciones que le fueron asignadas.
  *
- * `PaymentSplitter` follows a _pull payment_ model. This means that payments are not automatically forwarded to the
- * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the {release}
- * function.
+ * `PaymentSplitter` sigue un modelo _pull payment_. 
+ * Esto significa que los pagos no se reenvían automáticamente a las cuentas, 
+ * sino que se mantienen en este contrato, y la transferencia real se activa como 
+ * un paso separado al llamar a la función {liberación}.
  */
 contract PaymentSplitter is Context {
     event PayeeAdded(address account, uint256 shares);
